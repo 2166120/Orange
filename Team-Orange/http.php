3cd3191ef22e6c70a9eca580d613e14526be248a
Bryn Edades <bryn.edades@gmail.com>
2019-05-17 14:59:57 +0800

    added

---
+++ Team-Orange/http.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/animate.min.css" rel="stylesheet"> 
+  <link href="css/font-awesome.min.css" rel="stylesheet">
+  <link href="css/lightbox.css" rel="stylesheet">
+  <link href="css/main.css" rel="stylesheet">
+  <link id="css-preset" href="css/presets/preset1.css" rel="stylesheet">
+  <link href="css/responsive.css" rel="stylesheet">
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
+  <link rel="shortcut icon" href="images/favicon2.png">
+</head>
+<body>
+  <div id="intro">
+    <div class="container">
+      <section class="site-section bg-light">
+        <div class="container">
+            <div class="row justify-content-center mb-5">
+            <h1>Orange Quizzer</h1>
+            <div class="col-md-12 text-center">
+               <img src="images/orange.jpg" alt="Image placeholder">
+               <p class="lead">
+                <h1>HTTP</h1>
+                means HyperText Transfer Protocol. HTTP is the underlying protocol used by the World Wide Web and this protocol defines how messages are formatted and transmitted, and what actions Web servers and browsers should take in response to various commands.</p>
+
+            </div>
+            </div>
+            </div>
+            <h1>Choose a type of quiz</h1>
+            <div class="row blog-entries">
+                <div class="col-md-6 col-sm-6 col-12 wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">         
+                    <img src="images/red_question_mark_icon_vector_281131.jpg" alt="Image placeholder">
+                    <h2>Identification</h2>
+                     <a href="identification.php" class="blog-entry">
+                      <p>10 questions
+                    </a>
+                     <a href="identification.php" class="blog-entry">
+                      <p>20 questions
+                    </a>
+                     <a href="identification.php" class="blog-entry">
+                      <p>30 questions
+                    </a>
+                </div>       
+                <div class="col-md-6 col-sm-6 col-12 wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="400ms">
+                    <img src="images/mc.png" alt="Image placeholder">
+                    <h2>Multiple Choice</h2>
+                    <a href="multipleChoice.php" class="blog-entry">
+                      <p>10 questions
+                    </a>
+                     <a href="multipleChoice.php" class="blog-entry">
+                      <p>20 questions
+                    </a>
+                     <a href="multipleChoice.php" class="blog-entry">
+                      <p>30 questions
+                    </a>
+                </div>
+            </div>
+            </div>
+        </div>
+      </section>
+    </div>
+  <footer id="footer">
+    <div class="footer-bottom">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6">
+            <p>Copyright &copy; 2019 Team Orange | Web Systems & Technologies</p></p>
+          </div>
+          <div class="col-sm-6">
+            <p class="pull-right">All rights reserved.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</body>
+</html>
---
+++ Team-Orange/js.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/animate.min.css" rel="stylesheet"> 
+  <link href="css/font-awesome.min.css" rel="stylesheet">
+  <link href="css/lightbox.css" rel="stylesheet">
+  <link href="css/main.css" rel="stylesheet">
+  <link id="css-preset" href="css/presets/preset1.css" rel="stylesheet">
+  <link href="css/responsive.css" rel="stylesheet">
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
+  <link rel="shortcut icon" href="images/favicon2.png">
+</head>
+<body>
+  <div id="intro">
+    <div class="container">
+      <section class="site-section bg-light">
+        <div class="container">
+            <div class="row justify-content-center mb-5">
+            <h1>Orange Quizzer</h1>
+            <div class="col-md-12 text-center">
+               <img src="images/orange.jpg" alt="Image placeholder">
+               <p class="lead">
+                <h1>JS</h1>
+                Javascript is a scripting languages, primarily used on the Web. It is used to enhance HTML pages and is commonly found embedded in HTML code. JavaScript is an interpreted language. Thus, it doesn't need to be compiled. JavaScript renders web pages in an interactive and dynamic fashion. This allowing the pages to react to events, exhibit special effects, accept variable text, validate data, create cookies, detect a user’s browser, etc.</p>
+
+            </div>
+            </div>
+            </div>
+            <h1>Choose a type of quiz</h1>
+            <div class="row blog-entries">
+                <div class="col-md-6 col-sm-6 col-12 wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="300ms">         
+                    <img src="images/red_question_mark_icon_vector_281131.jpg" alt="Image placeholder">
+                    <h2>Identification</h2>
+                     <a href="identification.php" class="blog-entry">
+                      <p>10 questions
+                    </a>
+                     <a href="identification.php" class="blog-entry">
+                      <p>20 questions
+                    </a>
+                     <a href="identification.php" class="blog-entry">
+                      <p>30 questions
+                    </a>
+                </div>       
+                <div class="col-md-6 col-sm-6 col-12 wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="400ms">
+                    <img src="images/mc.png" alt="Image placeholder">
+                    <h2>Multiple Choice</h2>
+                    <a href="multipleChoice.php" class="blog-entry">
+                      <p>10 questions
+                    </a>
+                     <a href="multipleChoice.php" class="blog-entry">
+                      <p>20 questions
+                    </a>
+                     <a href="multipleChoice.php" class="blog-entry">
+                      <p>30 questions
+                    </a>
+                </div>
+            </div>
+            </div>
+        </div>
+      </section>
+    </div>
+  <footer id="footer">
+    <div class="footer-bottom">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6">
+            <p>Copyright &copy; 2019 Team Orange | Web Systems & Technologies</p></p>
+          </div>
+          <div class="col-sm-6">
+            <p class="pull-right">All rights reserved.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</body>
+</html>
