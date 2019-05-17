6c3df2b5784bce26b9ed1d9e3ecc7281af4a076f
Bryn Edades <bryn.edades@gmail.com>
2019-05-17 10:32:43 +0800

    updated

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/animate.min.css" rel="stylesheet"> 
+  <link href="css/font-awesome.min.css" rel="stylesheet">
+  <link href="css/lightbox.css" rel="stylesheet">
+  <link href="css/main.css" rel="stylesheet">
+  <link id="css-preset" href="css/presets/preset1.css" rel="stylesheet">
+  <link href="css/responsive.css" rel="stylesheet">
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
+  <link rel="shortcut icon" href="images/favicon2.png">
+</head>
+<body>
+  <div id="intro">
+    <div class="container">
+      <section class="site-section bg-light">
+        <div class="container">
+            <div class="row justify-content-center mb-5">
+              <h1>Orange Quizzer</h1>
+            <div class="col-md-12 text-center">
+
+            	 <img src="images/orange.jpg" alt="Image placeholder">
+                <p class="lead">
+                <h1>about.</h1>
+                OrangeQuizzer is an interactive website application that helps the users to learn and at the same time have fun in learning one topic each from HyperText Transfer Protocol (HTTP), Cascading Style Sheets (CSS) and JavaScript(JS). OrangeQuizzer provides good learning materials and quizzes. It also proide examples andd trivia for the users.</p>
+
+            </div>
+            </div>
+         
+                 <h1>Choose a topic</h1>
+
+                    <a href="index2.php" class="blog-entry">
+                    <img src="images/http.png" alt="Image placeholder">
+                    <h2>HyperText Transfer Protocol</h2>
+                    </a>
+                </div>
+                <div class="col-md-6 col-sm-6 col-12 wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="400ms">
+                    <a href="index2.php" class="blog-entry">
+                    <img src="images/css.png" alt="Image placeholder">
+                    <h2>Cascading Style Sheets</h2>
+                    </a>
+                </div>
+                 <div class="col-md-6 col-sm-6 col-12 wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="400ms">
+                    <a href="index2.php" class="blog-entry">
+                    <img src="images/js.png" alt="Image placeholder">
+                    <h2>JavaScript</h2>
+                    </a>
+                </div>
+           
+            </div>
+        </div>
+      </section>
+    </div>
+  <footer id="footer">
+    <div class="footer-bottom">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6">
+            <p>Copyright &copy; 2019 Team Orange | Web Systems & Technologies</p></p>
+          </div>
+          <div class="col-sm-6">
+            <p class="pull-right">All rights reserved.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</body>
+</html>
